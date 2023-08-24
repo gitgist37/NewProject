f226f5450a6237c961512c4fc16cfb8340fe09c0
gitgist37 <proventure37@gmail.com>
2023-08-24 14:11:13 +0530

    Added task 11 in NewProject on GitHuB

--- task11.js
+++ task11.js
@@ -3,6 +3,7 @@ var formTake = document.getElementById('demo');
 
 formTake.addEventListener('submit',copyData);
 
+
 function copyData(e)
 {
     e.preventDefault();
@@ -10,6 +11,8 @@ function copyData(e)
     var userEmail = document.getElementById('Email').value;
     var userContact = document.getElementById('Phone').value;
     var time = document.getElementById('board').value;
+    
+
 
     //console.log(userName,userEmail,userContact,time);
 
@@ -25,17 +28,39 @@ function copyData(e)
         'Time':time
 
     };
+
     
+    
+    let key = new Date().toLocaleString();
     var myObj_stringForm = JSON.stringify(myObj);
-    //console.log(myObj_serialized);
+    // //console.log(myObj_serialized);
 
 
-     localStorage.setItem('myObj',myObj_stringForm);
-     //console.log(localStorage);
+      localStorage.setItem(key,myObj_stringForm);
+      console.log(localStorage);
 
-     var myObj_objectForm = JSON.parse(myObj_stringForm);
-     console.log(myObj_objectForm);
+      
+
+
+     document.getElementById('Name').value = "";
+     document.getElementById('Email').value = "";
+     document.getElementById('Phone').value = "";
+     document.getElementById('board').value = "";
+
+    
+     
+
+
+
+    //  var myObj_objectForm = JSON.parse(myObj_stringForm);
+    //  console.log(myObj_objectForm);
+
+    // var myObj_objectForm = JSON.parse(localStorage.getItem("myObj"));
+    // myObj_objectForm.push();
+    // localStorage.setItem("myObj", JSON.stringify(myObj_objectForm));
 
 }
+
+//formTake.reset();
 
 
