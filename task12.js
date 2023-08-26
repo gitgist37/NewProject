e8f056e7ff99dbd609b0eef5901d28abb1a38da3
gitgist37 <proventure37@gmail.com>
2023-08-26 21:19:04 +0530

    Added task12.js and DOM_task10.html file in NewProject on GitHub

---
+++ task12.js
@@ -0,0 +1,69 @@
+var formTake = document.getElementById('demo');
+var captureuL = document.getElementById('displayUsers');
+
+//console.log(captureuL);
+
+
+formTake.addEventListener('submit',copyData);
+
+
+
+function copyData(e)
+{
+    e.preventDefault();
+    var userName = document.getElementById('Name').value;
+    var userEmail = document.getElementById('Email').value;
+    var userContact = document.getElementById('Phone').value;
+    var time = document.getElementById('board').value;
+    
+    var myObj = {
+        'Name':userName,
+        'Email':userEmail,
+        'Phone':userContact,
+        'Time':time
+
+    };
+
+    let key = new Date().toLocaleString();
+    var myObj_stringForm = JSON.stringify(myObj);
+    // //console.log(myObj_serialized);
+
+
+      localStorage.setItem(key,myObj_stringForm);
+      //console.log(localStorage);
+
+     var showUser = document.createElement('li');
+     showUser.textContent = "Name:" + userName + " Email:" + userEmail + " Contact-Number:" + userContact + " Time of Appointment:" + time;
+     showUser.style.color = "white";
+     
+
+     var DeleteBanao = document.createElement('button');
+     DeleteBanao.setAttribute('id','hatao');
+     DeleteBanao.textContent = 'remove';
+     showUser.appendChild(DeleteBanao);
+     
+     captureuL.appendChild(showUser);
+
+     //var removeItem = document.getElementById('hatao');
+     DeleteBanao.addEventListener('click',removeData);
+
+      
+
+
+     document.getElementById('Name').value = "";
+     document.getElementById('Email').value = "";
+     document.getElementById('Phone').value = "";
+     document.getElementById('board').value = "";
+
+     
+}
+
+function removeData(e)
+{
+    e.preventDefault();
+    e.target.parentNode.remove();
+    //console.log(a);
+    
+   
+}
+
