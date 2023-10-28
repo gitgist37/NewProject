9bdfb4d6609984de833914051fa7d3d0ce4cae66
gitgist37 <proventure37@gmail.com>
2023-10-28 14:23:09 +0530

    Added html and js files for promise all task

---
+++ promises_all_task.js
@@ -0,0 +1,83 @@
+const posts = [];
+let post_count=1;
+//let lastActive = new Date().toUTCString();
+
+// console.log(post2add);
+
+function getPost()  //SHOW
+{
+    if(!posts.length)
+    console.log('No posts to show');
+
+    posts.forEach((post) => 
+    {
+        console.log(post.title);
+    });
+}
+
+let a = function createPost()  // CREATE
+{
+    
+    return new Promise((resolve,reject)=>
+    {
+        
+        
+         setTimeout(()=>
+         {
+             resolve(posts[posts.push({title:'post'+ post_count++})-1].title);
+            
+         }, 1000);
+        
+    });
+}
+
+let b = function updateLastUserActivityTime()   //UPDATE TIME
+{
+    return new Promise((resolve,reject)=>
+    {
+        setTimeout(()=>
+        {
+            const lastActive = new Date().toUTCString();
+            resolve(lastActive);
+        }, 2000);
+        
+    });
+}
+
+let c = function deletePost()  // DELETE
+{
+    return new Promise((resolve,reject)=>
+    {
+        if(posts.length>0)
+        {
+            resolve(posts.pop());
+        }
+        
+    });
+}
+
+// createPost().then(updateLastUserActivityTime).
+// then(createPost).then(updateLastUserActivityTime).then(getPost);
+// createPost(updateLastUserActivityTime);
+Promise.all([a(),b()]).then((values)=>
+{
+    
+    console.log(values.reduce((m,n)=>m+"===>"+n));
+});
+Promise.all([a(),b()]).then((values)=>
+{
+    
+    console.log(values.reduce((m,n)=>m+"===>"+n));
+});
+Promise.all([a(),b()]).then((values)=>
+{
+    
+    console.log(values.reduce((m,n)=>m+"===>"+n));
+}).then(c).then(getPost);
+
+
+
+
+
+
+
